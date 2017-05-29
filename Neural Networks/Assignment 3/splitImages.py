0f72dd1914d7566ca501d843d7ea70b036e27f81
deepakks1995 <deepakks1995@gmail.com>
2017-05-29 15:36:22 +0530

    No change

--- Neural Networks/Assignment 3/splitImages.py
+++ Neural Networks/Assignment 3/splitImages.py
@@ -56,6 +56,49 @@ def split_train_test_data(percentage, data_path, test_path):
 	return train_data, test_data, train_data_labels, test_data_labels, class_weights
 
 
+def load_test_data():
+	load_data = []
+	load_data_labels = []
+	class_folders = os.listdir(os.getcwd() + "/" + data_path)
+	class_weights = {}
+	total_samples = 0
+	for cls in class_folders:
+		label = int(cls[5]) - 1
+		listings = os.listdir(os.getcwd() + "/" + data_path + "/" + cls)
+		class_weights[label] = len(listings)
+		total_samples = total_samples + len(listings)
+		one_hot_encoding = np.zeros(7)
+		one_hot_encoding[label] = 1
+		one_hot_encoding_list = [one_hot_encoding for _ in range(len(listings))]
+		i = 0
+		for file in listings:
+			img = image.load_img(os.getcwd() + "/" + data_path + "/" + cls + "/"  + file, target_size=(32, 32))
+			x = image.img_to_array(img)
+			load_data.append(x)
+			load_data_labels.append(one_hot_encoding_list[i])
+			i += 1
+	for cls in range(7):
+		class_weights[cls] = float(class_weights[cls]) / total_samples
+		print class_weights[cls],' No. of samples:',total_samples
+	shuff_indices = [_ for _ in range(len(load_data))]
+	random.shuffle(shuff_indices)
+	train_data = []
+	train_data_labels = [] 
+	test_data = []
+	test_data_labels = []
+	count = 0
+	for i in shuff_indices:
+		if count < percentage*len(load_data):
+			test_data.append(load_data[i])
+			test_data_labels.append(load_data_labels[i])
+		else:
+			train_data.append(load_data[i])
+			train_data_labels.append(load_data_labels[i])
+		count = count + 1
+	save_test_data(test_data, data_path, test_path)
+	return train_data, test_data, train_data_labels, test_data_labels, class_weights
+
+
 def save_test_data(test_data, data_path, test_path):
 	for i in range(len(test_data)):
 		img = Image.fromarray(test_data[i].astype('uint8'))
